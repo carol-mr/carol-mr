84d49ae07b8e2de7a44a1e1315414de3a5787528
carol-mr <mellocarol276@gmail.com>
2024-06-07 16:45:59 -0300

    classe carro adicionado

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Aula de git");
+class carro{
+    constructor(tipo , capacidade){
+        this.tipo = tipo;
+        this.capacidade = capacidade
+    }
+}
+
+const gol = new carro("carro",5);
